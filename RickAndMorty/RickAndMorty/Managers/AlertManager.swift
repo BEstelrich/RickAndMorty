d3a6325eb114e0a6ce87750fccdfbe6ef8c3cb6a
Bartolome Esterlich <b.estelrich@mycloudportion.com>
2019-08-01 12:09:50 -0400

    Final commit
    Checked app launches and runs as expected.

--- RickAndMorty/RickAndMorty/Managers/AlertManager.swift
+++ RickAndMorty/RickAndMorty/Managers/AlertManager.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class AlertManager: UIAlertController {
     
+    /// Shows an alert when there is no episodes.
     func showEpisodesErrorAlert() {
         let alert = UIAlertController(title: "No episodes error",
                                       message: "There is no episodes to show. Please try again.",
@@ -21,6 +22,7 @@ class AlertManager: UIAlertController {
         UIApplication.shared.delegate?.window??.rootViewController!.present(alert, animated: true)
     }
     
+    /// Shows an alert when there is no characters.
     func showCharactersErrorAlert() {
         let alert = UIAlertController(title: "No characters error",
                                       message: "There is no characters to show. Please try again.",
@@ -31,4 +33,5 @@ class AlertManager: UIAlertController {
         alert.addAction(action)
         UIApplication.shared.delegate?.window??.rootViewController!.present(alert, animated: true)
     }
+    
 }
